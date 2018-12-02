f45489b2dbf5bf58241d60b7b923b281344ed17c
Leesssssssss <772592971@qq.com>
2018-12-02 22:25:27 +0800

    2018-12-2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Users = require('./db.js')
 const app = express();
 app.use(bodyParser.json());
 
+// 跨域
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -15,27 +16,58 @@ app.all('*', function (req, res, next) {
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/getUserInfo', (req, res) => {
-  var user = req.body;
+// 存储用户openid
+app.post('/getOpenId', (req, res) => {
+  var openid = req.body;
 
-  var UserInfo = new Users(user);
+  var UserInfo = new Users(openid);
   Users.find({ openid: req.body.openid }).then(result => {
     if (result.length === 0) {
       UserInfo.save();
-    } else {
     }
   })
-  res.send('ok');
+  res.send('存储openid成功');
 })
 
+
+// 存储用户信息
+app.post('/getUserInfo', (req, res) => {
+  var openid = req.body.openid;
+  var userInfo = req.body.userInfo;
+
+  Users.updateOne({ openid: openid },
+    { $set: { userInfo: userInfo } }, (err) => {
+      // console.log(err);
+    })
+  res.send('存储用户信息成功');
+})
+
+// 存储用户备忘录
 app.post('/addNote', (req, res) => {
   var note = req.body;
-  console.log(note);
-  Users.updateOne({ openid: req.body.openid },
-    { $push: { note: note }},(err) => {
+  var openid = req.body.openid;
+  var top = req.body.top;
+
+  if (top === true) {
+    Users.update({ openid: openid, 'note.top': true }, { $set: { 'note.$.top': false } }, (err) => {
       console.log(err);
+    })
+  }
+  Users.updateOne({ openid: openid },
+    { $push: { note: note } }, (err) => {
+      // console.log(err);
     });
-  res.send('ok');
+  res.send('存储备忘录成功');
+})
+
+// 获取用户备忘录
+app.post('/getNote', (req, res) => {
+  var openid = req.body.openid;
+  var note = []
+  Users.find({ openid: openid }).then(result => {
+    note = result[0].note;
+    res.send(note);
+  })
 })
 
 
